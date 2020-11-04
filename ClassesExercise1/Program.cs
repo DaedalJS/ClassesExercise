39d4dcda2d0c800f7e391c82b08079a61611e091
Josh Strawbridge <HolyKnightJoshua@gmail.com>
2020-11-03 18:02:32 -0800

    first Classes exercise

---
+++ ClassesExercise1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Net.Http;
+
+namespace ClassesExercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Car1 = new Car("ford", "ranger", 2020);
+
+            Console.WriteLine($"{Car1.Year} {Car1.Make} {Car1.Model}");
+        }
+    }
+}
